97fd04a00bfcfd70997b0754303974e3bb5a082b
Farzaneh Haghani <68525882+farzaneh-haghani@users.noreply.github.com>
2022-12-13 01:59:47 +0000

    status-checker done

--- status-checker.js
+++ status-checker.js
@@ -1,13 +1,13 @@
 import { checkIfNoMovesLeft } from './board-printer.js';
 
-/*
-    Example board:
-        let board = [
-            ['X', '_', '_'],
-            ['_', 'X', '_'],
-            ['O', 'O', 'X']
-        ];
-*/
+
+// Example board:
+let board = [
+    ['X', '_', '_'],
+    ['_', 'X', '_'],
+    ['O', 'O', 'X']
+];
+
 
 /*
     Given 3 parameters:
@@ -17,7 +17,21 @@ import { checkIfNoMovesLeft } from './board-printer.js';
     Return true if the player has made a move in all 3 squares in the row
     Otherwise, return false
 */
+
+
+
 function checkRow(board, player, rowNumber) {
+
+    let xPlayer = [];
+    let oPlayer = [];
+    if (player === "X")
+        xPlayer = board[rowNumber].filter(x => x === 'X');
+    else
+        oPlayer = board[rowNumber].filter(x => x === 'O');
+
+    if (xPlayer.length === 3 || oPlayer.length === 3)
+        return true;
+    return false;
 }
 
 /*
@@ -29,7 +43,21 @@ function checkRow(board, player, rowNumber) {
     Otherwise, return false
 */
 function checkColumn(board, player, columnNumber) {
+
+    let xPlayer = [];
+    let oPlayer = [];
+
+    for (let i = 0; i < 3; i++)
+        if (player === "X" && board[i][columnNumber] === "X")
+            xPlayer.push("X");
+        else if (player === "O" && board[i][columnNumber] === "O")
+            oPlayer.push("O");
+
+    if (xPlayer.length === 3 || oPlayer.length === 3)
+        return true;
+    return false;
 }
+
 
 /*
     Given 2 parameters:
@@ -39,8 +67,46 @@ function checkColumn(board, player, columnNumber) {
     Otherwise, return false
 */
 function checkDiagonal(board, player) {
-    // It may be easier to use an if statement than a loop here
+
+    let xPlayer = [];
+    let oPlayer = [];
+    for (let i = 0; i < 3; i++) {
+        if (player === "X" && board[i][i] === "X")
+            xPlayer.push("X");
+        else if (player === "O" && board[i][i] === "O")
+            oPlayer.push("O");
+    }
+
+    if (xPlayer.length === 3 || oPlayer.length === 3)
+        return true;
+
+    else {
+        let xPlayer = [];
+        let oPlayer = [];
+
+        let k = 0;
+        for (let j = 2; j >= 0; j--) {
+
+            if (player === "X" && board[k][j] === "X")
+                xPlayer.push("X");
+            else if (player === "O" && board[k][j] === "O")
+                oPlayer.push("O");
+
+            k++;
+        }
+
+        if (xPlayer.length === 3 || oPlayer.length === 3)
+            return true;
+        else
+            return false;
+    }
+
 }
+
+
+
+// It may be easier to use an if statement than a loop here
+
 
 
 /*
@@ -48,13 +114,13 @@ function checkDiagonal(board, player) {
 */
 
 function checkIfPlayerWon(board, player) {
-    for(let i = 0; i <= 2; i++) {
-        if(checkRow(board, player, i) || checkColumn(board, player, i)) {
+    for (let i = 0; i <= 2; i++) {
+        if (checkRow(board, player, i) || checkColumn(board, player, i)) {
             return true;
         }
     }
 
-    if(checkDiagonal(board, player)) {
+    if (checkDiagonal(board, player)) {
         return true;
     }
 
@@ -62,17 +128,17 @@ function checkIfPlayerWon(board, player) {
 }
 
 export function isGameOver(board) {
-    if(checkIfPlayerWon(board, 'X')) {
+    if (checkIfPlayerWon(board, 'X')) {
         console.log('X has won the game!\n');
         return true;
     }
 
-    if(checkIfPlayerWon(board, 'O')) {
+    if (checkIfPlayerWon(board, 'O')) {
         console.log('O has won the game!\n');
         return true;
     }
 
-    if(checkIfNoMovesLeft(board)) {
+    if (checkIfNoMovesLeft(board)) {
         console.log('Game Over - It\s a tie!\n');
         return true;
     }
