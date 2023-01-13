8e7da60ead1d35a3b9a4d4b7332965da7b2d5f41
ZahraaTayyar <zahraatayyar@gmail.com>
2023-01-13 22:01:15 +0000

    Update board-printer.js

--- board-printer.js
+++ board-printer.js
@@ -15,8 +15,33 @@
         =================
     Test your function by calling it with an example tic-tac-toe board.
 */
+
+let board = [
+            
+            ['x', 'X', 'x'],
+            ['O', 'O', 'X'],
+            ['X', '0', '0'],
+];
+
+function hasEmptyCells(board){
+    let emptyCellsCount = 0;
+
+    for (let )
+
 export function printBoard(board) {
+    const firstRow = board[0];
+    const secondRow = board[1];
+    const thirdRow = board[2];
+
+    console.log(`  ${firstRow[0]}  |  ${firstRow[1]}   |  ${firstRow[2]}  `);
+    console.log("==================");
+    console.log(`  ${secondRow[0]}  |  ${secondRow[1]}   |  ${secondRow[2]}  `);
+    console.log("==================");
+    console.log(`  ${thirdRow[0]}  |  ${thirdRow[1]}   |  ${thirdRow[2]}  `);
+    console.log("==================");
 }
+
+printBoard(board)
 
 /*
     Given a tic-tac-toe board (an array of arrays),
@@ -24,4 +49,13 @@ export function printBoard(board) {
         - return false if there are still moves that can be made
 */
 export function checkIfNoMovesLeft(board) {
+    let emptyCellsCount = 0;
+
+    for (let row of board) {
+        let emptyCells = row.filter(cellValue => cellValue === "_");
+        emptyCellsCount = emptyCellsCount + emptyCells.length
+    }
+    return emptyCellsCount === 0;
 }
+
+console.log(checkIfNoMovesLeft(board));
